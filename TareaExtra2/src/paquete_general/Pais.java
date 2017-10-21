2fea7e4d5534a8e74f8f09e7184373631ac1318d
Diego Merino <diegomerino@MacBook-Pro-de-Diego.local>
2017-10-21 16:57:32 -0500

    tareaExtra2

---
+++ TareaExtra2/src/paquete_general/Pais.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_general;
+
+/**
+ *
+ * @author diegomerino
+ */
+public class Pais {
+    private String nombre;
+    
+    public void establecer_nombre(String n){
+        nombre = n;
+    }
+    public String obtener_nombre(){
+        return nombre;
+    }
+    @Override
+    public String toString (){
+        String cadena = String.format("%s",obtener_nombre());
+        return cadena;
+    }
+}
